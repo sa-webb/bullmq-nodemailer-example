c2346ca8af7484db97188739e11014b9601078f5
Austin Webb <sawebb30@gmail.com>
2021-01-14 15:35:13 -0500

    feat: worker2

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,3 +3,7 @@ import { Queue } from "bullmq";
 import connection from "../connection";
 
 export const sendMailQueue = new Queue("worker1", { connection });
+
+export const messegeQueue = new Queue("worker2", {
+  connection: connection.duplicate(),
+});
